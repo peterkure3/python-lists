6edb16a1e0fa4a21649bd614fe0cd84d524a3b4f
peterkure3 <peterkure256@gmail.com>
2022-06-28 11:34:00 +0300

    Adding lists file via upload

---
+++ lists.py
@@ -0,0 +1,63 @@
+# Lists
+# A list is a relationship of indices and elements
+# A map between an element and its postion
+
+[1,2,3,4,5,6] # list of integers
+
+["Gensi",12 ,45 ,78 ,'a'] #list of strings and integers
+
+coffee = ["arabica", "robusta", "morgus", "liberica"]
+
+type(coffee)
+
+# working with a list that has numbers
+# printing out the elements
+
+# numbers = [1,2,3,4,5]
+# for num in numbers:
+#     print(num)
+
+# print("")
+
+# updating a list
+# numbers = [69,27,32,45,28]
+# for marks in range(len(numbers)):
+#     marks = marks + 10
+#     print(marks)
+
+
+
+# numbers = [69,27,32,45,28]
+# for marks in numbers:
+#     marks = marks + 10
+#     print(marks)
+
+
+
+
+#creating an empty list
+empty=[]
+
+for i in empty:
+    print(i)
+
+# adding lists using concactenation operators
+num1 = [1,2,3,4]
+num2 =[ 6,7,8,9]
+num3 = num1 + num2
+print(num3)
+
+# multiplying lists by integers
+num4 = [1,2]
+num5 = num4 * 2
+print(num5)
+
+# working with indices in lists
+cs_performance = [56,78,87,96,57,90,67]
+first_3 = cs_performance[1:3]
+print(first_3)
+
+print(cs_performance[4:])
+
+print(cs_performance[3:6])
+
